1f0b7bed91dfc622a322b11ec392fd81a663bfb3
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 01:10:55 +0530

    error 3

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-on-heroku'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/openweathermap/index.html'));});
+    '/dist/openweathermap'));});
 app.listen(process.env.PORT || 8080);
