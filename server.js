ea93948cf2f206ae7048d75d04163928a80af36a
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 23:04:33 +0530

    fix

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.all(function (req, res, next) {
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 app.use(express.static(__dirname + '/dist/openweathermap'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
