be196542a5c49c729f687d8dcfba677e4e2e43b4
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 14:08:16 +0530

    nuk

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var cors = require('cors')
 
-//CORS middleware
-var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', config.allowedDomains);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-//...
 app.configure(function () {
 
-  app.use(allowCrossDomain);
+  app.use(cors());
   app.use(express.static(__dirname + '/dist/openweathermap'));
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-      '/dist/openweathermap'));
-  });
-  app.listen(process.env.PORT || 8080);
 });
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+    '/dist/openweathermap'));
+});
+app.listen(process.env.PORT || 8080);
 
