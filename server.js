66b5102cd943ff980ed2518da24e8e40ec760d8b
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 11:10:13 +0530

    error6

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
+// const express = require('express');
+// const path = require('path');
+// const cors = require('cors');
+
+// const app = express();
+// app.use(express.static(__dirname + '/dist/openweathermap'));
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join(__dirname+
+//     '/dist/openweathermap'));});
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/openweathermap'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/openweathermap'));});
+
+// add this code
+const whitelist = ['http://localhost:4200']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
+
+app.use(express.static('./dist/openweathermap'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/openweathermap'));
+});
+
 app.listen(process.env.PORT || 8080);
