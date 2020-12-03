4586552677ccf0b33ea84be43904b12f2c0e242e
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 15:42:26 +0530

    nuk

--- src/app/apicall.service.ts
+++ src/app/apicall.service.ts
@@ -11,7 +11,7 @@ export class ApicallService {
   getWeatherData(cityname: string): Promise<any> {
     return new Promise((resolve, reject) => {
       const apiKey: string = "dd9b0500dd6b376e09a395ca5de98ebe";
-      let weatherUrl: string = "http://api.openweathermap.org/data/2.5/weather?q=" + cityname.trim() + "&appid=" + apiKey;
+      let weatherUrl: string = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=" + cityname.trim() + "&appid=" + apiKey;
 
       this.http.get(weatherUrl).toPromise()
         .then(resolve)
