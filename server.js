f7991d336e2a57836378cc2843f6966341715d8f
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 15:33:34 +0530

    nuk

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.all(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(express.static(__dirname + '/dist/openweathermap'));
-app.use(cors());
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/openweathermap'));});
