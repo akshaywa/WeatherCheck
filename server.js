384a18f33e8acbf61166377a2a2b8a7f239d6273
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 14:25:36 +0530

    nuk

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-var cors = require('cors')
 
-app.configure(function () {
-
-  app.use(cors());
-  app.use(express.static(__dirname + '/dist/openweathermap'));
-});
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname +
-    '/dist/openweathermap'));
-});
+app.use(express.static(__dirname + '/dist/openweathermap'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/openweathermap'));});
 app.listen(process.env.PORT || 8080);
-
