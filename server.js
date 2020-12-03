f87e42d0209e7e9095c7699381f26c461e018df9
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2020-12-03 13:54:25 +0530

    error7

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-on-heroku'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/openweathermap'));});
-app.listen(process.env.PORT || 8080);
+
+//CORS middleware
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', config.allowedDomains);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+//...
+app.configure(function () {
+
+  app.use(allowCrossDomain);
+  app.use(express.static(__dirname + '/dist/openweathermap'));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+      '/dist/openweathermap'));
+  });
+  app.listen(process.env.PORT || 8080);
+});
+
